e9e5f02f76bb305cd004f8ae95d5bdd925cc536e
Ed Hazledine <ed.hazledine@gmail.com>
2019-06-25 21:00:30 +0100

    Refactor rotate

--- 8/rotate.py
+++ 8/rotate.py
@@ -2,6 +2,4 @@ def rotate(string, n):
     """Rotate characters in a string.
        Expects string and n (int) for number of characters to move.
     """
-    string_list = list(string)
-    return "".join(string_list[n:] + string_list[:n])
-
+    return string[n:] + string[:n]
